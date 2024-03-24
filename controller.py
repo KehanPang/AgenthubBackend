0c0d5f711ddd6400c585e08eeefc38a9eaaf3d4c
Khancat1120 <1755675467@qq.com>
2024-03-25 01:38:26 +0800

    fixerror

--- controller.py
+++ controller.py
@@ -1,266 +1,210 @@
-# 导入所需的模块和库
-import fastapi
-from fastapi import Request
-from service import BLL
+from dao import DAO
+from zhipuai import ZhipuAI
 
-# 创建FastAPI应用
-app = fastapi.FastAPI()
+class BusinessLogicLayer:
+    def __init__(self):
+        self.DAO = DAO
+        api_key = "34fdbb9c8ce06a02a21aeed9e6b685c6.CMOwSsZwJoUc8W4Z"
+        self.clinet = ZhipuAI(api_key=api_key) # 填写您自己的APIKey
 
-
-# 用户注册接口
-@app.post("/register")
-async def register(register_form: Request):
-    """
-        Expected input: {
-            "username": 用户名,
-            "password": 密码
+    # 用户注册接口
+    def register(self, register_form):
+        return {
+            "code": self.DAO.add_new_user(
+                register_form['username'], 
+                register_form['password']    
+            )
         }
+    
+    # 用户登录接口
+    def login(self, login_form):
+        return {
+            "code": self.DAO.login(
+                login_form['username'], 
+                login_form['password']
+            )
+        }
+
+    # 聊天接口
+    def chat(self, chat_form):
+        agent_to_be_used = self.DAO.find_agent_by_id(chat_form['agent_id'])
         
-        Expected return: {
-            "code": 200: 正常 | 400: 权限不足 | 403: 用户名重复
-        }
-    """
-    register_form = await register_form.json()
-    # print(data)
-    # > {'username': 'test', 'password': 'test'}
-    return BLL.register(register_form)
-
-
-# 用户登录接口
-@app.post("/login")
-async def login(login_form: Request):
-    """
-        Expected input: {
-            "username": 用户名,
-            "password": 密码
-        }
+        if agent_to_be_used == None:
+            return {"code": 404} # 智能体不存在
         
-        Expected return: {
-            "code": 200: 正常 | 401: 密码错误 | 404: 用户不存在
-        }
-    """
-    login_form = await login_form.json()
-    return BLL.login(login_form)
-
-
-# 聊天接口
-@app.post("/chat")
-async def chat(chat_form: Request):
-    """
-        Expected input: {
-            "username": 用户名,
-            "agent_id": 智能体id,
-            "message": 用户与智能体的全部对话, 对话内容需要遵从指定格式, 如下面的message
-        }
-        "message": {
-            "text": [
-                {"role": "user", "content": "用户说的第一句话"},
-                {"role": "assistant", "content": "AI说的第一句话"},
-                {"role": "user", "content": "用户说的第二句话"},
-                {"role": "assistant", "content": "AI说的第二句话"}
-            ]
-        }
+        if chat_form['username'] != agent_to_be_used.username_belongs_to and agent_to_be_used.agent_type != "OFFICIAL_LEVEL":
+            return {"code": 400} # 权限不足
         
-        Expected return: {
-            "code": 200: 正常 | 400: 权限不足 | 404: 智能体不存在
-            "massage": 智能体回复给用户的一句话, 同样需要遵循上文的message格式
-        }
-    """
-    chat_form = await chat_form.json()
-    return BLL.chat(chat_form)
-
-
-# 查找用户接口(管理员接口)
-@app.post("/find_user")
-async def find_user(find_user_form: Request):
-    """
-        Expected input: {
-            "username": 用户名
-        }
+        agent_prompt = agent_to_be_used.prompt
+        final_prompt = agent_prompt + chat_form['message']['text']
         
-        Expected return: {
-            "code": 200: 正常 | 404: 用户不存在,
-            "username": 用户名,
-            "password": 用户密码,
-            "user_level": 用户级别，需要在["ADMIN", "USER"]里取值
-        }
-    """
-    find_user_form = await find_user_form.json()
-    return BLL.find_user(find_user_form)
-
-
-# 查找所有用户接口(管理员接口)
-@app.post("/find_all_users")
-async def find_all_users(find_all_users_form: Request):
-    """
-        Expected input: {
-            "username": 用户名, 必须是ADMIN, 否则会返回400
-        }
+        content = ""
+        try:
+            response = self.clinet.chat.completions.create(
+                model="glm-4",  # 填写需要调用的模型名称
+                messages=final_prompt,
+            )
+            content = response.choices[0].message.content
+        except:
+            content = "这个话题不太合适，我们换个话题吧。"
         
-        Expected return: {
-            "code": 200: 正常 | 400: 权限不足 | 404: 用户不存在
-            "users": [
+        return {
+                    "code": 200,
+                    "username": chat_form["username"],
+                    "agent_id": chat_form["agent_id"],
+                    "message": {
+                    'text': [
+                        {"role": "assistant", "content": content}
+                        ]
+                    }
+        }
+
+
+    # 查找用户接口
+    def find_user(self, find_user_form):
+        user = self.DAO.find_user_by_username(find_user_form["username"])
+        
+        if user == None:
+            return {"code": 404}
+        
+        return {
+            "code": 200,
+            "username": user.username,
+            "password": user.password,
+            "user_level": user.user_level
+        }
+
+    # 查找所有用户接口
+    def find_all_users(self, find_all_users_form):
+        if find_all_users_form["username"] != "ADMIN":
+            return {"code": 400}
+        
+        users = self.DAO.find_all_users()
+        if users == None:
+            return {"code": 404}
+        
+        user_list = []
+        for user in users:
+            user_list.append(
                 {
-                    "username": 用户名,
-                    "password": 用户密码,
-                    "user_level": 用户级别，需要在["ADMIN", "USER"]里取值
-                },
-                ……
-            ]
+                    "username": user.username,
+                    "password": user.password,
+                    "user_level": user.user_level
+                }
+            )
+            
+        return {
+            "code": 200,
+            "users": user_list
         }
-    """
-    find_all_users_form = await find_all_users_form.json()
-    return BLL.find_all_users(find_all_users_form)
 
 
-# 查找智能体接口
-@app.post("/find_agent")
-async def find_agent(find_agent_form: Request):
-    """
-        Expected input: {
-            "username": 用户名, 必须是ADMIN, 否则会返回400,
-            "agent_id: 智能体id
-        }
+    # 查找智能体接口
+    def find_agent(self, find_agent_form):
+        agent = self.DAO.find_agent_by_id(find_agent_form['agent_id'])
         
-        Expected return: {
-            "code": 200: 正常 | 400: 权限不足 | 404: 智能体不存在
-            "agent_id": 智能体id,
-            "agent_name": 智能体名,
-            "prompt": 智能体的prompt, 如果是共有智能体的则会返回None,
-            "agent_type": 智能体类型, 取值于["OFFICIAL_LEVEL", "USER_LEVEL"],
+        if agent == None:
+            return {"code": 404}
+        
+        if find_agent_form['username'] != agent.username_belongs_to and agent.agent_type != "OFFICIAL_LEVEL":
+            return {"code": 400} # 权限不足
+        
+        return {
+            "code": 200, #200表示正常 400表示智能体不存在 401表示用户权限不足
+            "agent_id": agent.agent_id,
+            "agent_name": agent.agent_name,
+            "prompt": "" if agent.agent_type == "OFFICIAL_LEVEL" else agent.prompt,
+            "agent_type": agent.agent_type,
             "username_belongs_to": agent.username_belongs_to
         }
-    """
-    find_agent_form = await find_agent_form.json()
-    return BLL.find_agent(find_agent_form)
 
-
-# 查找所有智能体接口
-@app.post("/find_all_agents")
-async def find_all_agents(find_all_agents_form: Request):
-    """
-        Expected input: {
-            "username": 用户名, 必须是ADMIN, 否则会返回400
-        }
+    # 查找所有智能体接口
+    def find_all_agents(self, find_all_agents_form):
+        if find_all_agents_form["username"] != "ADMIN":
+            return {"code": 400}
         
-        Expected return: {
-            "code": 200: 正常 | 400: 权限不足 | 404: 智能体不存在
-            "users": [
+        agents = self.DAO.find_all_agents()
+        if agents == None:
+            return {"code": 404}
+        
+        agent_list = []
+        for agent in agents:
+            agent_list.append(
                 {
-                    "agent_id": 智能体id,
-                    "agent_name": 智能体名,
-                    "prompt": 智能体的prompt,
-                    "agent_type": 智能体类型, 取值于["OFFICIAL_LEVEL", "USER_LEVEL"],
+                    "agent_id": agent.agent_id,
+                    "agent_name": agent.agent_name,
+                    "prompt": agent.prompt,
+                    "agent_type": agent.agent_type,
                     "username_belongs_to": agent.username_belongs_to
-                },
-                ……
-                
-            ]
-        }
-    """
-    find_all_agents_form = await find_all_agents_form.json()
-    return BLL.find_all_agents(find_all_agents_form)
-
-
-# 添加智能体接口
-@app.post("/add_agent")
-async def add_agent(add_agent_form: Request):
-    """
-        Expected input: {
-            "agent_name": 智能体名
-            "agent_type": ['OFFICIAL_LEVEL', 'USER_LEVEL']
-            "prompt": prompt格式与message相同
-            "username_belongs_to": 动作发起用户, 管理员是ADMIN
+                }
+            )
+        return {
+            "code": 200,
+            "users": agent_list
         }
         
-        Expected return: {
-            "code": 200: 正常 | 400: 权限不足 | 403: 智能体已存在
+    def update_user(self, update_user_form):
+        return {
+            "code": self.DAO.update_user_password(
+                update_user_form['username'], update_user_form['password']
+            )
         }
-    """
-    add_agent_form = await add_agent_form.json()
-    return BLL.add_agent(add_agent_form)
 
-
-# 更新智能体接口
-@app.post("/update_agent")
-async def update_agent(update_agent_form: Request):
-    """
-        Expected input: {
-            "agent_id": 智能体id,
-            "agent_name": 智能体名,
-            "prompt": prompt格式与message相同,
+    # 添加智能体接口
+    def add_agent(self, add_agent_form):
+        return {
+                "code": self.DAO.add_new_agent(
+                    add_agent_form["agent_name"],
+                    add_agent_form["agent_type"],
+                    add_agent_form["prompt"],
+                    add_agent_form["username_belongs_to"]
+                )
         }
+
+    # 更新智能体接口
+    def update_agent(self, update_agent_form):
+        return {
+                "code": self.DAO.update_agent(
+                    update_agent_form["agent_id"],
+                    update_agent_form["username"],
+                    update_agent_form["agent_name"],
+                    update_agent_form["prompt"]
+                )
+        }
+
+    # 删除智能体接口
+    def delete_agent(self, delete_agent_form):
+        return {
+                "code": self.DAO.delete_agent(
+                    delete_agent_form["username_belongs_to"],
+                    delete_agent_form["agent_id"]
+                )
+        }
+    
+    # 返回用户可见的智能体接口
+    def find_available_agents(self, find_available_agents_form):
+        if find_available_agents_form["username"] != "ADMIN":
+            return {"code": 400}
         
-        Expected return: {
-            "code": 200: 正常 | 400: 权限不足 | 404: 智能体不存在 | 403: 数据库异常
-        }
-    """
-    update_agent_form = await update_agent_form.json()
-    return BLL.update_agent(update_agent_form)
-
-
-# 删除智能体接口
-@app.post("/delete_agent")
-async def delete_agent(delete_agent_form: Request):
-    """
-        Expected input: {
-            "agent_id": 智能体id,
-            "username": 用户名,
-        }
+        agents = self.DAO.find_user_available_agents(find_available_agents_form["username"])
+        if agents == None:
+            return {"code": 404}
         
-        Expected return: {
-            "code": 200: 正常 | 400: 权限不足 | 404: 智能体不存在 | 403: 数据库异常
-        }
-    """
-    delete_agent_form = await delete_agent_form.json()
-    return BLL.delete_agent(delete_agent_form)
-
-
-# 更新用户接口
-@app.post("/update_user")
-async def update_user(update_user_form: Request):
-    """
-        Expected input: {
-            "username": 用户名
-            "password": 密码
-        }
-        
-        Expected return: {
-            "code": 200: 正常 | 404: 用户不存在
-        }
-    """
-    update_user_form = await update_user_form.json()
-    return BLL.update_user(update_user_form)
-
-
-# 查找用户可见的接口
-@app.post("/find_available_agents")
-async def update_user(find_available_agents_form: Request):
-    """
-        Expected input: {
-            "username": 用户名, 必须是ADMIN, 否则会返回400
-        }
-        
-        Expected return: {
-            "code": 200: 正常 | 400: 权限不足 | 404: 用户不存在,
-            "users": [
+        agent_list = []
+        for agent in agents:
+            agent_list.append(
                 {
-                    "agent_id": 智能体id,
-                    "agent_name": 智能体名,
-                    "prompt": 智能体的prompt, 如果是官方的智能体会返回None
-                    "agent_type": 智能体类型, 取值于["OFFICIAL_LEVEL", "USER_LEVEL"],
+                    "agent_id": agent.agent_id,
+                    "agent_name": agent.agent_name,
+                    "prompt": None if agent.agent.agent_type == "OFFICIAL_LEVEL" else agent.prompt,
+                    "agent_type": agent.agent_type,
                     "username_belongs_to": agent.username_belongs_to
-                },
-                ……
-                
-            ]
+                }
+            )
+        return {
+            "code": 200,
+            "users": agent_list
         }
-    """
-    find_available_agents_form = await find_available_agents_form.json()
-    return BLL.find_available_agents(find_available_agents_form)
 
 
-# 运行FastAPI应用
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8888)
+BLL = BusinessLogicLayer()
